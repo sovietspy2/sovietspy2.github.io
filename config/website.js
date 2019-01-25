6d9d3e4b665f29034c6fb33ed2e73b7bc4523ced
SovietSpy2 <sovietspy2@yandex.com>
2019-01-25 12:30:59 +0100

    init

---
+++ config/website.js
@@ -0,0 +1,24 @@
+const tailwind = require('../tailwind')
+
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
+
+  siteTitle: 'Barnabas Hang \'s portfolio', // Navigation and Site Title
+  siteTitleAlt: 'Barnabas Hang', // Alternative Site title for SEO
+  siteTitleShort: 'Hang', // short_name for manifest
+  siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
+  siteUrl: 'https://sovietspy2.github.io', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/logo.png', // Used for SEO and manifest
+  siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
+  author: 'LekoArts', // Author for schema.org JSONLD
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '@cara', // Twitter Username
+  ogSiteName: 'cara', // Facebook Site Name
+  ogLanguage: 'en_US', // Facebook Language
+
+  // Manifest and Progress color
+  themeColor: tailwind.colors.orange,
+  backgroundColor: tailwind.colors.blue,
+}
